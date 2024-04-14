65ff9f47b3dfac09f11a5ddf26e324b68196f88e
tettle2806 <zidanzohid@gmail.com>
2024-04-14 15:08:39 +0500

    oop 1

--- oop/1.py
+++ oop/1.py
@@ -3,5 +3,18 @@ class Point:
     color = 'red'
     circle = 2
 
-    def set_coords():
-        print('Вызов метода set_coords')
+    def set_coords(self):
+        print(f'Вызов метода set_coords {self}')
+        print(type(self))
+        """
+            Грубо говоря параметр self это ссылка которая указывает экземпляр класса то есть
+            Без регистрации объекта функция set_coords не будет работать
+            
+            регистрация экземпляра класса pt = Point() и дальнейший вызов функции set_coords 
+            без регистрации функция не будет работать Point.set_coords() выдаст ошибку
+            
+            TypeError: Point.set_coords() missing 1 required positional argument: 'self'
+        """
+
+
+
